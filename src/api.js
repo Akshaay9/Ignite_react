332c8890856eab65b1ff1b4d26b8eab7b0c4a27d
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2020-11-30 14:18:21 +0530

    able to fetch all different games

---
+++ src/api.js
@@ -0,0 +1,49 @@
+// Base URL
+
+const base_url=`https://api.rawg.io/api/`;
+
+
+// base month
+const getCurrentMonth=()=>{
+    const month=new Date().getMonth()+1
+    if(month<10)
+    {
+        return `0${month}`
+    }
+    else
+    {
+        return month
+    }
+};
+// base date
+const getCurrentDay=()=>{
+    const date=new Date().getDate();
+    if(date<10)
+    {
+        return `0${date}`
+    }
+    else
+    {
+        return date
+    }
+};
+// year
+const currentYear=new Date().getFullYear();
+const currentMonth=getCurrentMonth()
+const currentDay=getCurrentDay()
+// complete date
+
+const currentDate=`${currentYear}-${currentMonth}-${currentDay}`
+const lastYear=`${currentYear-1}-${currentMonth}-${currentDay}`
+const nextYear=`${currentYear+1}-${currentMonth}-${currentDay}`
+
+const popularGames=`games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const upcoming_games=`games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
+const newGames=`games?dates=${currentDate},${nextYear}&ordering=-released&page_size=10`
+
+export const newGamesUrl=()=>`${base_url}${newGames}`
+export const upcomingGamesUrl=()=>`${base_url}${upcoming_games}`
+export const popularGamesUrl=()=>`${base_url}${popularGames}`
+  
+
+
